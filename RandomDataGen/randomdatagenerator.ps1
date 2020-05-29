ac1e8dbe8ea216930c8fb0b2f28102453d60e0b1
Scott Dowling <spdowling@gmail.com>
2020-05-29 14:09:13 -0400

    Added hiredate

--- RandomDataGen/randomdatagenerator.ps1
+++ RandomDataGen/randomdatagenerator.ps1
@@ -1,14 +1,41 @@
 # Random data generator
-$header = "userId,isBC,isClicker,isEUA"
+
+Function Get-RandomDate {
+    [cmdletbinding()]
+
+    param(
+        [DateTime]
+        $Min,
+
+        [DateTime]
+        $Max = [DateTime]::Now
+    )
+
+    Begin{
+        If(!$Min -or !$Max){
+            Write-Warning "Unable to parse entered string for Min or Max parameter. Proper example: `"06/23/1996 14:06:03.297`""
+            Write-Warning "Time will default to midnight if omitted"
+            Break
+        }
+    }
+
+    Process{
+        $randomTicks = Get-Random -Minimum $Min.Ticks -Maximum $Max.Ticks
+        New-Object DateTime($randomTicks)
+    }
+}
+
+$header = "userId,hireDate,isBC,isClicker,isEUA"
 
 $header | Out-File -FilePath C:\Repositories\ScriptMigo\Non-Project-Code\random.csv -Append 
 
 for ($i = 0; $i -lt 2501; $i++) {
     $user = "User" + $i
+    $hireDate = (Get-Randomdate -min "06/23/1996 14:06:03.297").tostring("MM-dd-yyyy")
     $value1 = 0,1 | Get-Random
     $value2 = 0,1 | Get-Random
     $value3 = 0,1 | Get-Random
     
-    $outString = $user + "," + $value1 + "," + $value2+ "," + $value3
+    $outString = $user + "," + $hireDate + "," + $value1 + "," + $value2+ "," + $value3
     $outString | Out-File -FilePath C:\Repositories\ScriptMigo\Non-Project-Code\random.csv -Append 
 }
